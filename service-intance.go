dda51022c4661ced0cf79ea9310a258ca0e876da
kingdo777 <1440852110@qq.com>
2019-12-13 06:49:00 +0800

    计算实例的最大并发值 v3 添加一些打印信息

--- service-intance.go
+++ service-intance.go
@@ -41,6 +41,7 @@ func runToGetData(SLO time.Duration, deploymentsClient v1.DeploymentInterface, u
 		concurrencyIndex := 0
 		for conc = concurrency[concurrencyIndex]; conc <= concurrency[len(concurrency)-1]; {
 			latency = sendRequest(url, conc, runTime)
+			fmt.Printf("request1:conc=%d and latency=%f\n", end, latency)
 			if latency < SecondSLO {
 				concurrencyIndex++
 				conc = concurrency[concurrencyIndex]
@@ -61,15 +62,18 @@ func runToGetData(SLO time.Duration, deploymentsClient v1.DeploymentInterface, u
 		//此时已经确定，最佳并发在concurrency[concurrencyIndex-1]到concurrency[concurrencyIndex]之间
 		start := concurrency[concurrencyIndex-1]
 		end := concurrency[concurrencyIndex] - 1
+		fmt.Printf("bestConc between in %d and %d\n", start, end)
 		for conc = (start + end) / 2; start < end; conc = (start + end) / 2 {
 			if conc == start {
 				latency = sendRequest(url, end, runTime)
+				fmt.Printf("request2:conc=%d and latency=%f\n", end, latency)
 				if latency < SecondSLO {
 					conc = end
 				}
 				break
 			} else {
 				latency = sendRequest(url, conc, runTime)
+				fmt.Printf("request3:conc=%d and latency=%f\n", end, latency)
 				if latency < SecondSLO {
 					start = conc
 				} else {
