a880c214fbe897c6d17fe42a29222d535170c797
kingdo777 <1440852110@qq.com>
2019-12-13 06:52:08 +0800

    计算实例的最大并发值 v4 削减大部分测试用例

--- service-intance.go
+++ service-intance.go
@@ -14,7 +14,8 @@ const (
 
 var (
 	concurrency = [...]int{
-		1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
+		//1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
+		1, 2, 4, 8, 16,
 	}
 	CostPerformanceTable = [len(vmConfigList)][len(concurrency)]float64{}
 )
