9b0892572dbf9f905b7679d7dae73fe440b9fce2
kingdo777 <1440852110@qq.com>
2019-12-14 06:06:28 +0800

    减少VM个数，太多会导致内存溢出

--- test.go
+++ test.go
@@ -1,7 +1,5 @@
 package main
 
-import "github.com/ewalker544/libsvm-go"
-
 //const (
 //	TrainDataFilePath = "data/train"
 //)
@@ -18,31 +16,31 @@ func main0() {
 	//svmTrain("data/train.vm1")
 }
 
-func svmTrain(trainDataFile string) string {
-	param := libSvm.NewParameter() // Create a parameter object with default values
-	param.KernelType = libSvm.POLY // Use the polynomial kernel
-
-	model := libSvm.NewModel(param) // Create a model object from the parameter attributes
-
-	// Create a problem specification from the training data and parameter attributes
-	problem, _ := libSvm.NewProblem(trainDataFile, param)
-
-	model.Train(problem) // Train the model from the problem specification
-
-	modelFile := trainDataFile + ".model"
-
-	model.Dump(modelFile)
-	return modelFile
-}
-
-func svmPredicting(modelFile string) float64 {
-	// Create a model object from the model file generated from training
-	model := libSvm.NewModelFromFile(modelFile)
-
-	x := make(map[int]float64)
-	// Populate x with the test vector
-
-	predictLabel := model.Predict(x) // Predicts a float64 label given the test vector
-
-	return predictLabel
-}
+//func svmTrain(trainDataFile string) string {
+//	param := libSvm.NewParameter() // Create a parameter object with default values
+//	param.KernelType = libSvm.POLY // Use the polynomial kernel
+//
+//	model := libSvm.NewModel(param) // Create a model object from the parameter attributes
+//
+//	// Create a problem specification from the training data and parameter attributes
+//	problem, _ := libSvm.NewProblem(trainDataFile, param)
+//
+//	model.Train(problem) // Train the model from the problem specification
+//
+//	modelFile := trainDataFile + ".model"
+//
+//	model.Dump(modelFile)
+//	return modelFile
+//}
+//
+//func svmPredicting(modelFile string) float64 {
+//	// Create a model object from the model file generated from training
+//	model := libSvm.NewModelFromFile(modelFile)
+//
+//	x := make(map[int]float64)
+//	// Populate x with the test vector
+//
+//	predictLabel := model.Predict(x) // Predicts a float64 label given the test vector
+//
+//	return predictLabel
+//}
