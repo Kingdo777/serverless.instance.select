ef025d35aa6abdf3b3620c22ca77a8cedf32b5d3
kingdo777 <1440852110@qq.com>
2019-12-14 04:37:32 +0800

    去掉tets.go

--- test.go
+++ test.go
@@ -4,7 +4,7 @@ package main
 //	TrainDataFilePath = "data/train"
 //)
 
-func main() {
+func maidn() {
 	for i := 0; i < 100; i++ {
 		makeTrainData(i, 0.1*float64(i), TrainDataFilePath+".vm")
 	}
@@ -12,7 +12,7 @@ func main() {
 
 //func makeTrainData(conc int, latency float64, filename string) {
 //	//每执行一次，添加一次
-//	fp, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+//	fp, err := os.OpenFile(filename, os.O_WRqONLY|os.O_CREATE|os.O_APPEND, 0777)
 //	if err != nil {
 //		fmt.Println(err)
 //		return
