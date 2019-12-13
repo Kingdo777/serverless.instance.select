fc495a68b2745b115689d838ba7a8675e685207d
kingdo777 <1440852110@qq.com>
2019-12-14 04:04:27 +0800

    修改生成训练数据集的代码v3 添加test.go

--- test.go
+++ test.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-//"fmt"
-//"os"
+	//"fmt"
+	//"os"
+	"fmt"
+	"os"
 )
 
-//const (
-//	TrainDataFilePath = "data/train"
-//)
+const (
+	TrainDataFilePath = "data/train"
+)
 
-func maini() {
+func main() {
 	for i := 0; i < 100; i++ {
-		print(TrainDataFilePath + ".vm")
 		makeTrainData(i, 0.1*float64(i), TrainDataFilePath+".vm")
 	}
 }
 
-//func makeTrainData(conc int, latency float64, filename string) {
-//	//每执行一次，添加一次
-//	fp, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE, 6)
-//	defer fp.Close()
-//	if err != nil {
-//		fmt.Println(err)
-//		return
-//	}
-//	fp.WriteString(fmt.Sprintf("%d %f\n", conc, latency))
-//}
+func makeTrainData(conc int, latency float64, filename string) {
+	//每执行一次，添加一次
+	fp, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE, 6)
+	defer fp.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fp.WriteString(fmt.Sprintf("%d %f\n", conc, latency))
+}
