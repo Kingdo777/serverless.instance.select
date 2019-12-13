79d626c7fb21b31406cc427af445c9210d245874
kingdo777 <1440852110@qq.com>
2019-12-14 03:59:03 +0800

    修改生成训练数据集的代码v3 修改0777

--- svm-handle.go
+++ svm-handle.go
@@ -8,7 +8,7 @@ import (
 
 func makeTrainData(conc int, latency float64, filename string) {
 	//每执行一次，添加一次
-	fp, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE, 0666)
+	fp, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE, 0777)
 	defer fp.Close()
 	if err != nil {
 		fmt.Println(err)
