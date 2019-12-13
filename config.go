70380b518cb289638948658a55b26d75827e61ea
kingdo777 <1440852110@qq.com>
2019-12-13 08:40:18 +0800

    添加了除model之外的其他SI信息 V5 修改SI没有用指针作为参数的Bug 调大并发度，不然没效果

--- config.go
+++ config.go
@@ -96,7 +96,7 @@ var (
 var (
 	concurrency = [...]int{
 		//1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
-		1, 2, 4, 8, 16,
+		1, 2, 4, 8, 16, 32, 64, 128,
 	}
 	CostPerformanceTable = [len(vmConfigList)][len(concurrency)]float64{}
 )
